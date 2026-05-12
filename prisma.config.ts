821efeb02929aafe3522219582153b797fd11a86
seo-hash <seo@madroom.it>
2026-05-12 14:08:37 +0200

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,8 +2,9 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  // Aggiungi il ./ davanti ai percorsi
+  schema: "./prisma/schema.prisma", 
   migrations: {
-    path: "prisma/migrations",
+    path: "./prisma/migrations",
   },
 });
